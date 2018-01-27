e46df73708b2a1768ae66f9587e37dbb905f4e36
Satchel Grant <grantsrb@gmail.com>
2018-01-27 15:29:40 -0800

    add untested text generation function to RecurrentUnit

--- RecurrentUnit.py
+++ RecurrentUnit.py
@@ -7,12 +7,13 @@ import DoubleGRU
 
 
 class RecurrentUnit(nn.Module):
-    def __init__(self, n_words, emb_size, state_size=256):
+    def __init__(self, n_words, emb_size, state_size=256, seq_len=30):
         super(RecurrentUnit, self).__init__()
 
         self.n_words = n_words
         self.emb_size = emb_size
         self.state_size = state_size
+        self.seq_len = seq_len
 
         # Embedding Sets
         means = torch.zeros(n_words,emb_size)
@@ -40,6 +41,7 @@ class RecurrentUnit(nn.Module):
         self.sigmoid = nn.Sigmoid()
         self.tanh = nn.Tanh()
         self.relu = nn.ReLU()
+        self.softmax = nn.Softmax()
 
     def forward(self, old_h, emb_idxs):
         """
@@ -61,3 +63,37 @@ class RecurrentUnit(nn.Module):
         output = exit_x.mm(self.classifier)
 
         return h, output
+
+    def generate_text(self, seed, generation_len):
+        """
+        Generates text conditioned on the seed.
+
+        seed - LongTensor of length > sequence length that was used for training.
+                Ideally prepend a few '\0' characters to the seed.
+        """
+        
+        # Build h
+        h = Variable(torch.zeros(1,self.state_size))
+        for i in range(len(seed)):
+            h, output = self.forward(Variable(h.data), Variable(seed[j:j+1]))
+
+        # Generate Text
+        generated_idxs = torch.zeros(seed.size(0)+generation_len).long()
+        generated_idxs[:seed.size(0)] = seed
+        _, new_idx = torch.max(self.softmax(output), 0)
+        generated_idxs[seed.size(0)] = new_idx.data[0]
+        for i in range(seed.size(0)+1,generated_idxs.size(0)):
+            h, output = self.forward(Variable(h.data), Variable(generated_text[i-1:i]))
+            _, new_idx = torch.max(self.softmax(output),0)
+            generated_idxs[i] = new_idx.data[0]
+        return generated_idxs
+        
+    
+    def verbalize(self, semantic_tensor):
+        """
+        This function seeks to describe in a sentence a tensor that contains semantics
+        within the scope of the training data.
+        """
+        return None 
+                    
+
