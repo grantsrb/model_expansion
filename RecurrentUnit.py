879d8ba8a236afc7da2341f3bea579739b011f94
Satchel Grant <grantsrb@gmail.com>
2018-01-29 21:22:17 -0800

    add verbalize function

--- RecurrentUnit.py
+++ RecurrentUnit.py
@@ -29,18 +29,19 @@ class RecurrentUnit(nn.Module):
 
         # Exit Parameters
         self.exit_bnorm = nn.BatchNorm1d(state_size)
-        means = torch.zeros(state_size, state_size)
+        means = torch.zeros(state_size, emb_size)
         self.exit = nn.Parameter(torch.normal(means, std=1/float(np.sqrt(state_size))), requires_grad=True)
 
         # Classifier
-        self.classifier_bnorm = nn.BatchNorm1d(state_size)
-        means = torch.zeros(state_size, n_words)
-        self.classifier = nn.Parameter(torch.normal(means, std=1/float(np.sqrt(state_size))), requires_grad=True)
+        self.classifier_bnorm = nn.BatchNorm1d(emb_size)
+        means = torch.zeros(emb_size, n_words)
+        self.classifier = nn.Parameter(torch.normal(means, std=1/float(np.sqrt(emb_size))), requires_grad=True)
 
         # Non Linear Activation Functions
         self.sigmoid = nn.Sigmoid()
         self.tanh = nn.Tanh()
         self.relu = nn.ReLU()
+        self.mse = nn.MSELoss()
         self.softmax = nn.Softmax(-1)
 
     def forward(self, old_h, emb_idxs):
@@ -48,7 +49,20 @@ class RecurrentUnit(nn.Module):
         old_h - FloatTensor Variable with dimensions (batch_size, state_size)
         emb_idxs - LongTensor Variable with dimensions (batch_size,)
         """
+
         embeddings = self.embeddings[emb_idxs]
+
+        h, exit_x = self.subforward(old_h, embeddings)
+
+        output = self.classifier_bnorm(exit_x)
+        output = exit_x.mm(self.classifier)
+
+        return h, output
+
+    def subforward(self, old_h, embeddings):
+        """
+        Helper function for forward
+        """
 
         embeddings = self.entry_bnorm(embeddings)
         x = self.relu(embeddings.mm(self.entry))
@@ -59,14 +73,11 @@ class RecurrentUnit(nn.Module):
         exit_x = self.exit_bnorm(h)
         exit_x = self.relu(exit_x.mm(self.exit))
 
-        output = self.classifier_bnorm(exit_x)
-        output = exit_x.mm(self.classifier)
-
-        return h, output
+        return h, exit_x
 
     def generate_text(self, seed, generation_len):
         """
-        Generates text conditioned on the seed.
+        Generates text conditioned on the seed. Returns indexes rather than actual text.
 
         seed - LongTensor of length > sequence length that was used for training.
                 Ideally prepend a few '\0' characters to the seed.
@@ -91,11 +102,54 @@ class RecurrentUnit(nn.Module):
         return generated_idxs
         
     
-    def verbalize(self, semantic_tensor):
+    def verbalize(self, semantic_tensor, sentence_len, lr=0.01, start_std=0.01, epochs=50):
         """
         This function seeks to describe in a sentence a tensor that contains semantics
-        within the scope of the training data.
-        """
-        return None 
-                    
+        within the scope of the training data. Returns embedding indexes rather than actual text.
 
+        semantic_tensor - FloatTensor to be verbalized (in a single sentence)
+        sentence_len - length of desired generated sentence
+        lr - learning rate for sentence updates
+        start_std - starting standard deviation for sentence matrix
+        epochs - num training epochs for sentence training
+        """
+
+        h = Variable(torch.zeros(1,state_size).float())
+        means = torch.zeros(sentence_size,self.emb_size)
+        sentence = Variable(torch.normal(means, std=start_std), requires_grad=True)
+        for p in self.parameters():
+            p.requires_grad = False
+            
+        for epoch in range(epochs):
+            exit_sum = Variable(torch.zeros(1,self.emb_size))
+            for i in range(sentence_len):
+                h, exit_x = self.subforward(Variable(h.data),sentence)
+                exit_sum += exit_x
+            loss = self.mse(exit_sum, Variable(semantic_tensor))
+            loss.backward()
+            sentence.data = sentence.data - lr*sentence.data.grad
+
+        idxs = []
+        for sent in sentence.data:
+            idxs.append(self.closest_emb_idx(sent))
+
+        for p in self.parameters():
+            p.requires_grad = True
+
+        return idxs 
+                    
+    def closest_emb_idx(self, tensor):
+        """
+        Finds closest embedding tensor to the argued tensor. Distance is guaged using 
+        mean squared difference.
+
+        tensor - FloatTensor with shape (self.emb_size,) that will be compared with each embedding tensor
+        """
+        tensor = tensor.squeeze()
+        smallest_mse = torch.sum((tensor-self.embeddings.data[1])**2)
+        emb_idx = 0
+        for i,emb in enumerate(self.embeddings.data[1:]: 
+            mse = torch.sum((tensor-emb)**2)
+            if mse < smallest_mse:
+                emb_idx = i
+        return emb_idx
